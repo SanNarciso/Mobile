4a6b22beecddfc6253e62ded46c875401c4fddfd
Simoffsky <maks-kirsanov26@mail.ru>
2023-01-11 16:03:43 +1000

    added TaskListener + favorite notes logic

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.model.task.Task
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TasksListener {
 
     lateinit var binding: ActivityMainBinding
 
-    private var adapter = TasksAdapter()
+    private var adapter = TasksAdapter(this)
+    private var tasks = mutableListOf<Task>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,11 @@ class MainActivity : AppCompatActivity() {
         fetchData()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList("KEY", tasks as ArrayList<Task>)
+    }
+
     private fun initRecyclerView() {
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
@@ -29,7 +35,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchData() {
-        val tasks = mutableListOf<Task>()
         tasks.add(Task(true, "Task 0", true))
         for (i in 1..10) {
             tasks.add(
@@ -37,6 +42,17 @@ class MainActivity : AppCompatActivity() {
             )
         }
         adapter.tasks = tasks
+    }
+
+    override fun updateTask(task: Task) {
+        //todo change text to id
+        tasks.forEachIndexed { ind, t ->
+            if (t.text == task.text) tasks[ind] = task
+        }
+    }
+
+    override fun removeTask(task: Task) {
+        tasks.remove(task)
     }
 
 }
--- app/src/main/java/com/example/todoapp/TaskAdapter.kt
+++ app/src/main/java/com/example/todoapp/TaskAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.databinding.TaskItemBinding
 import com.example.todoapp.model.task.Task
 
-class TasksAdapter : RecyclerView.Adapter<TasksAdapter.TasksViewHolder>() {
+class TasksAdapter(private val listener: TasksListener) : RecyclerView.Adapter<TasksAdapter.TasksViewHolder>() {
 
     var tasks: List<Task> = mutableListOf()
         set(newValue) {
@@ -17,19 +17,33 @@ class TasksAdapter : RecyclerView.Adapter<TasksAdapter.TasksViewHolder>() {
             }
         }
 
-    class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private val binding = TaskItemBinding.bind(view)
 
-        fun bind(task: Task) {
-            binding.isCompletedCheckBox.isChecked = task.isCompleted
-            binding.isCompletedCheckBox.text = task.text
+        fun bind(task: Task) = binding.run {
+
+            isCompletedCheckBox.isChecked = task.isCompleted
+            isCompletedCheckBox.text = task.text
+
+            isCompletedCheckBox.setOnClickListener {
+                task.isCompleted = isCompletedCheckBox.isChecked
+                listener.updateTask(task)
+                notifyDataSetChanged()
+            }
 
             val imageRes = if (task.isFavorite) R.drawable.ic_star else R.drawable.ic_star_border
-            binding.isFavouriteImageButton.setImageResource(imageRes)
+            isFavouriteImageButton.setImageResource(imageRes)
+
+            isFavouriteImageButton.setOnClickListener {
+                task.isFavorite = !task.isFavorite
+                listener.updateTask(task)
+                val imRes = if (task.isFavorite) R.drawable.ic_star else R.drawable.ic_star_border
+                isFavouriteImageButton.setImageResource(imRes)
+                notifyDataSetChanged()
+            }
 
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
