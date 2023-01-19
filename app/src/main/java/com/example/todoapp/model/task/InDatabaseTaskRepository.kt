47c14b7550b8e4015dbef2dd0c4837c17b32d816
Simoffsky <maks-kirsanov26@mail.ru>
2023-01-19 14:55:45 +1000

    moved logic to DataBaseTaskRepository and added observer

--- app/src/main/java/com/example/todoapp/model/task/InDatabaseTaskRepository.kt
+++ app/src/main/java/com/example/todoapp/model/task/InDatabaseTaskRepository.kt
@@ -4,18 +4,38 @@ class InDatabaseTaskRepository : TaskRepository {
 
     private var tasks: MutableList<Task> =  mutableListOf()
 
+    private val subscribers = mutableListOf<Observer>()
+
     override fun getTasks(): List<Task> = tasks as List<Task>
 
     override fun updateTask(task: Task) {
-
+        //todo change text to id
+        tasks.forEachIndexed { ind, t ->
+            if (t.text == task.text) tasks[ind] = task
+        }
+        notifyDataSetChanges()
     }
 
     override fun removeTask(task: Task) {
-
+        tasks.remove(task)
+        notifyDataSetChanges()
     }
 
     override fun add(task: Task) {
+        tasks.add(task)
+        notifyDataSetChanges()
+    }
 
+    fun addSubscriber(subscriber: Observer) {
+        subscribers.add(subscriber)
+    }
+
+    fun removeSubscriber(subscriber: Observer) {
+        subscribers.remove(subscriber)
+    }
+
+    private fun notifyDataSetChanges() {
+        subscribers.forEach { it.setChanges(tasks) }
     }
 
     companion object {
--- app/src/main/java/com/example/todoapp/views/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/views/tasks/TasksViewModel.kt
@@ -3,25 +3,40 @@ package com.example.todoapp.views.tasks
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.todoapp.model.task.InDatabaseTaskRepository
+import com.example.todoapp.model.task.Observer
 import com.example.todoapp.model.task.Task
 
-class TasksViewModel : ViewModel(), TasksListener {
+class TasksViewModel : ViewModel(), TasksListener, Observer {
 
-    private val _tasks = MutableLiveData<MutableList<Task>>()
-    val tasks: LiveData<MutableList<Task>> = _tasks
+    private val taskRepository = InDatabaseTaskRepository.get()
+
+    private val _tasks = MutableLiveData<List<Task>>()
+    val tasks: LiveData<List<Task>> = _tasks
+
+    init {
+        taskRepository.addSubscriber(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        taskRepository.removeSubscriber(this)
+    }
 
     override fun updateTask(task: Task) {
-        val list = _tasks.value ?: return
-        list.forEachIndexed { ind, t ->
-            if (t.text == task.text) list[ind] = task
-        }
-        _tasks.value = list
+        taskRepository.updateTask(task)
     }
 
     override fun removeTask(task: Task) {
-        val list = _tasks.value ?: return
-        list.remove(task)
-        _tasks.value = list
+        taskRepository.removeTask(task)
+    }
+
+    fun createTask() {
+        taskRepository.add(Task(false, "task", false))
+    }
+
+    override fun setChanges(tasks: List<Task>) {
+        _tasks.value = tasks
     }
 
     fun initState(state: MutableList<Task>? = null) {
@@ -29,25 +44,12 @@ class TasksViewModel : ViewModel(), TasksListener {
             _tasks.value = state!!
             return
         }
-        val tasks = mutableListOf<Task>()
-        tasks.add(Task(true, "Task 0", true))
+        taskRepository.add(Task(true, "Task 0", true))
         for (i in 1..10) {
-            tasks.add(
+            taskRepository.add(
                 Task(false, "Task $i", false)
             )
         }
-        _tasks.value = tasks
-    }
-
-    fun createTask() {
-        val list = _tasks.value ?: mutableListOf()
-        list.add(
-            Task(
-                isCompleted = false,
-                text = "Task ${list.size}",
-                isFavorite = false)
-        )
-        _tasks.value = list
     }
 
 
