1c73a9bbea3f48b3e166a3767e86f70be358a4c7
Simoffsky <maks-kirsanov26@mail.ru>
2023-01-19 15:40:32 +1000

    added dialog for creating task

--- app/src/main/java/com/example/todoapp/views/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todoapp/views/tasks/TasksFragment.kt
@@ -5,18 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.databinding.CreateTaskBottomSheetBinding
 
 import com.example.todoapp.databinding.FragmentTasksBinding
 import com.example.todoapp.model.task.Task
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class TasksFragment : Fragment() {
 
     private lateinit var binding: FragmentTasksBinding
     private val viewModel: TasksViewModel by activityViewModels()
     private lateinit var adapter: TasksAdapter
+    private lateinit var newTaskDialog: BottomSheetDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,17 +50,20 @@ class TasksFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         adapter = TasksAdapter(viewModel as TasksListener)
+        newTaskDialog = BottomSheetDialog(requireContext())
+        newTaskDialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
 
         initRecyclerView()
+        createTaskDialog()
 
         binding.createItemFab.setOnClickListener {
-            viewModel.createTask()
+//            viewModel.createTask()
+            newTaskDialog.show()
         }
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             adapter.tasks = it
         }
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -71,6 +78,17 @@ class TasksFragment : Fragment() {
         val layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = adapter
+    }
+
+    private fun createTaskDialog() {
+
+        val dialogBinding = CreateTaskBottomSheetBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+        dialogBinding.saveTaskButton.setOnClickListener {
+            newTaskDialog.dismiss()
+        }
+
+        newTaskDialog.setContentView(dialogBinding.root)
+
     }
 
     companion object {
