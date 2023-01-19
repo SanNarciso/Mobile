4bac9ff73dd16e68f4df18aca9fa8ed2f7aec051
Simoffsky <maks-kirsanov26@mail.ru>
2023-01-19 14:41:28 +1000

    adeed task repository

---
+++ app/src/main/java/com/example/todoapp/App.kt
@@ -0,0 +1,16 @@
+package com.example.todoapp
+
+import android.app.Application
+import com.example.todoapp.model.task.InDatabaseTaskRepository
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        InDatabaseTaskRepository.initial()
+
+
+    }
+
+}
